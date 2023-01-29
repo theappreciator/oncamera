4d4ff48a6567a890cdc37c57fd981624223e8ffc
Jesse Tucker <jetucker@paypal.com>
2023-01-29 11:01:00 -0500

    Setup MDNS publisher within status-api

--- packages/status-api/src/server.ts
+++ packages/status-api/src/server.ts
@@ -1,16 +1,19 @@
 import express, {Express, Request, Response} from "express";
 import Persist from "./persist";
+import { PERSIST_STORE_KEY, WebcamStatus, MdnsPublisher } from '@oncamera/common';
 
 const app: Express = express();
 const port = 9124;
 
 const persist = Persist.Instance;
 
-const KEYS = {
-    webcamStatus: "webcam.status",
-    online: "webcam.status.online",
-    offline: "webcam.status.offline"
-}
+const webcamServiceName = '_webcam_status._tcp.local';
+const publisher = new MdnsPublisher(
+    webcamServiceName,
+    "Webcam Status Server",
+    (json: any) => {
+        console.log("STATUS SERVER heartbeat");
+    });
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +23,7 @@ app.get("/", (req: Request, res: Response) => {
 })
 
 app.get("/api/webcam/status", (req: Request, res: Response) => {
-    const status = persist.retrieve(KEYS.webcamStatus) || KEYS.offline;
+    const status = persist.retrieve(PERSIST_STORE_KEY) || WebcamStatus.offline;
     res.send({status});
 })
 
@@ -28,18 +31,18 @@ app.post("/api/webcam/status", (req: Request, res: Response) => {
 
     const newStatus = req.body.status;
 
-    if (newStatus && (newStatus === KEYS.online || newStatus === KEYS.offline)) {
+    if (newStatus && (newStatus === WebcamStatus.online || newStatus === WebcamStatus.offline)) {
 
-        const oldStatus = persist.retrieve(KEYS.webcamStatus);
+        const oldStatus = persist.retrieve(PERSIST_STORE_KEY);
         console.log("Status - old: " + oldStatus + " new: " + newStatus);
 
         if (oldStatus !== newStatus) {
-            persist.save(KEYS.webcamStatus, newStatus);
-            console.log("Saved new status: ", persist.retrieve(KEYS.webcamStatus));
+            persist.save(PERSIST_STORE_KEY, newStatus);
+            console.log("Saved new status: ", persist.retrieve(PERSIST_STORE_KEY));
         }
     }
     
-    const status = persist.retrieve(KEYS.webcamStatus) || KEYS.offline;
+    const status = persist.retrieve(PERSIST_STORE_KEY) || WebcamStatus.offline;
     res.send({status});
 })
 
