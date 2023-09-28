34106a474ce1eab6e99ddb5e02500dd0f64707b0
Jess Tucker <jessjesstheappreciator@gmail.com>
2023-09-28 10:13:03 -0400

    Changing to offline now has a 5s delay, so lights don't flash

--- packages/status-api/source/constants.ts
+++ packages/status-api/source/constants.ts
@@ -1,1 +1,3 @@
-export const PERSIST_STORE_KEY = "webcam.status";
+export const PERSIST_STORE_STATUS_KEY = "webcam.status";
+export const PERSIST_STORE_TRANSITIONING_KEY = "webcam.transitioning";
+export const TRANSITIONING_TIME_MILLIS = 5000;
--- packages/status-api/tests/app.test.ts
+++ packages/status-api/tests/app.test.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { container, Lifecycle } from "tsyringe";
 const request = require('supertest');
-import { PERSIST_STORE_KEY } from "../source/constants";
+import { PERSIST_STORE_STATUS_KEY, PERSIST_STORE_TRANSITIONING_KEY, TRANSITIONING_TIME_MILLIS } from "../source/constants";
 import PersistService from "../source/services/persistService";
 import { DataKeys, WebcamStatus } from '@oncamera/common';
 
@@ -11,7 +11,7 @@ import { DataKeys, WebcamStatus } from '@oncamera/common';
 const app = require('../source/app');
 const persist = PersistService.Instance;
 
-describe('GET /', function() {
+describe('base App', function() {
     it('should responds with Hello World', (done) => {
       request(app)
         .get('/')
@@ -29,9 +29,20 @@ describe('GET /', function() {
         }, done);
     });
 
+    it('should return a webcam online status from the saved store', (done) => {
+
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.online);
+
+      request(app)
+        .get('/api/webcam/status')
+        .expect(200, {
+          status: WebcamStatus.online
+        }, done);
+    });
+
     it('should return a webcam offline status from the saved store', (done) => {
 
-      persist.save(PERSIST_STORE_KEY, WebcamStatus.offline);
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.offline);
 
       request(app)
         .get('/api/webcam/status')
@@ -40,7 +51,7 @@ describe('GET /', function() {
         }, done);
     });
 
-    it('should be able to update the saved webcam status to an empty store', (done) => {
+    it('should be able to update the saved webcam to online status to an empty store', (done) => {
 
       persist.clearAll();
 
@@ -52,9 +63,21 @@ describe('GET /', function() {
         }, done);
     });
 
-    it('should be able to update the same value to saved webcam status to a non-empty store', (done) => {
+    it('should be able to update the saved webcam to offline status to an empty store', (done) => {
 
-      persist.save(PERSIST_STORE_KEY, WebcamStatus.offline);
+      persist.clearAll();
+
+      request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.offline)
+        .expect(200, {
+          status: WebcamStatus.offline
+        }, done);
+    });
+
+    it('should be able to update the same value to saved webcam offline status to a non-empty store', (done) => {
+
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.offline);
       
       request(app)
         .post('/api/webcam/status')
@@ -64,9 +87,9 @@ describe('GET /', function() {
         }, done);
     });
 
-    it('should be able to update a different value to saved webcam status to a non-empty store', (done) => {
+    it('should be able to update the same value to saved webcam online status to a non-empty store', (done) => {
 
-      persist.save(PERSIST_STORE_KEY, WebcamStatus.offline);
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.online);
       
       request(app)
         .post('/api/webcam/status')
@@ -76,7 +99,120 @@ describe('GET /', function() {
         }, done);
     });
 
+    it('should be able to update status from offline->online', (done) => {
+
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.offline);
+      
+      request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.online)
+        .expect(200, {
+          status: WebcamStatus.online
+        }, done);
+    });
+
+    it('should be able to update status from online->offline, not already transitioning', async () => {
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.online);
+      
+      jest.useFakeTimers();
+
+      await request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.offline)
+        .expect(200, {
+          status: WebcamStatus.online
+        });
+      
+      jest.advanceTimersByTime(TRANSITIONING_TIME_MILLIS + 1);
+
+      const status = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(status).toBe(WebcamStatus.offline);
+    });
+
+    it('should be able to update status from online->offline, already transitioning', async () => {
+      const previousTimerId = '123456';
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.online);
+      persist.save(PERSIST_STORE_TRANSITIONING_KEY, previousTimerId);
+      
+      jest.useFakeTimers();
+
+      const clearTimeoutSpy = jest.spyOn(global, 'clearTimeout');
+
+      await request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.offline)
+        .expect(200, {
+          status: WebcamStatus.online
+        });
+
+      expect(clearTimeoutSpy).toHaveBeenCalledWith(previousTimerId);
+
+      const timerId = persist.retrieve(PERSIST_STORE_TRANSITIONING_KEY);
+      expect(timerId).not.toBe(previousTimerId);
+
+      jest.advanceTimersByTime(TRANSITIONING_TIME_MILLIS - 1);
+
+      const transitioningStatus = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(transitioningStatus).toBe(WebcamStatus.online);
+
+      jest.advanceTimersByTime(TRANSITIONING_TIME_MILLIS + 1);
+
+      const finalStatus = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(finalStatus).toBe(WebcamStatus.offline);
+    });
+
+    it('should be able to update status from online->offline, already transitioning, multiple requests', async () => {
+      const previousTimerId = '123456';
+      persist.save(PERSIST_STORE_STATUS_KEY, WebcamStatus.online);
+      persist.save(PERSIST_STORE_TRANSITIONING_KEY, previousTimerId);
+      
+      jest.useFakeTimers();
+
+      const clearTimeoutSpy = jest.spyOn(global, 'clearTimeout');
+
+      await request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.offline)
+        .expect(200, {
+          status: WebcamStatus.online
+        });
+
+      expect(clearTimeoutSpy).toHaveBeenCalledWith(previousTimerId);
+
+      const firstRequestTimerId = persist.retrieve(PERSIST_STORE_TRANSITIONING_KEY);
+      expect(firstRequestTimerId).not.toBe(previousTimerId);
+
+      jest.advanceTimersByTime(Math.floor(TRANSITIONING_TIME_MILLIS - 100));
+
+      const firstRequestTransitioningStatus = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(firstRequestTransitioningStatus).toBe(WebcamStatus.online);
+
+      await request(app)
+        .post('/api/webcam/status')
+        .send('status=' + WebcamStatus.offline)
+        .expect(200, {
+          status: WebcamStatus.online
+        });
+      
+      expect(clearTimeoutSpy).toHaveBeenCalledWith(firstRequestTimerId);
+
+      const secondRequestTimerId = persist.retrieve(PERSIST_STORE_TRANSITIONING_KEY);
+      expect(secondRequestTimerId).not.toBe(firstRequestTimerId);
+
+      jest.advanceTimersByTime(Math.floor(TRANSITIONING_TIME_MILLIS - 100));
+
+      const secondRequestTransitioningStatus = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(secondRequestTransitioningStatus).toBe(WebcamStatus.online);
+
+      jest.advanceTimersByTime(TRANSITIONING_TIME_MILLIS + 1);
+
+      const finalStatus = persist.retrieve(PERSIST_STORE_STATUS_KEY);
+      expect(finalStatus).toBe(WebcamStatus.offline);
+    });
+
     afterAll(() => {
       app.close();
+      persist.clearAll();
+      jest.useRealTimers();
     });
   });
